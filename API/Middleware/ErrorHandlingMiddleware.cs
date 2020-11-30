e5f1edc917a5fa94269dd407ff891ea156ad1778
Phillip <phillip.trinh1@gmail.com>
2020-12-01 08:10:58 +1100

    Add error handling middleware

--- API/Middleware/ErrorHandlingMiddleware.cs
+++ API/Middleware/ErrorHandlingMiddleware.cs
@@ -22,7 +22,7 @@ namespace API.Middleware
         {
             try
             {
-                await _next(context)
+                await _next(context);
             }
             catch (Exception e)
             {
